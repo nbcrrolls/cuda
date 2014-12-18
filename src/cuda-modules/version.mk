4c0408834a3ef86d1664481694508a7f2b11098b
nadyawilliams <nadya@sdsc.edu>
2014-12-18 09:29:53 -0800

    add cuda module, sets path for binaries, libs, man pages

---
+++ src/cuda-modules/version.mk
@@ -0,0 +1,9 @@
+# Get the Openmpi version from it's version.mk file. See Makefile
+VERSION.MK.MASTER = version.mk
+VERSION.MK.MASTER.DIR = ../nvidia-toolkit
+VERSION.MK.INCLUDE = toolkit.version.mk
+
+include $(VERSION.MK.INCLUDE) 
+NAME		= cuda-module
+RELEASE		= 0
+RPM.REQUIRES	= environment-modules
