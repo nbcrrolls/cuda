fd39869b9addba33deb88e005b03a7e01d3f26ed
nadyawilliams <nadya@sdsc.edu>
2014-12-18 09:55:49 -0800

    add nvidia driver version to the module info

--- src/cuda-modules/version.mk
+++ src/cuda-modules/version.mk
@@ -1,6 +1,7 @@
 # Get the Openmpi version from it's version.mk file. See Makefile
 VERSION.MK.MASTER = version.mk
 VERSION.MK.MASTER.DIR = ../nvidia-toolkit
+VERSION.MK.DRIVER.DIR = ../nvidia-driver
 VERSION.MK.INCLUDE = toolkit.version.mk
 
 include $(VERSION.MK.INCLUDE) 
