cd2c4ef19b25b7b5e6be35ead765e8a78a9902d5
nadyawilliams <nadya@sdsc.edu>
2016-04-15 18:18:55 -0700

    update driver to 361.28

--- src/nvidia-driver/version.mk
+++ src/nvidia-driver/version.mk
@@ -1,5 +1,5 @@
 NAME             = cuda-nvidia-driver
-VERSION          = 346.59
-RELEASE          = 0
+VERSION          = 361.28
+RELEASE          = 1
 DRIVER_INSTALLER = NVIDIA-Linux-x86_64-$(VERSION).run
 
