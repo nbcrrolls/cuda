22dd3f1f263d0e3d40a6c6a070c78b6be0e852b2
nadyawilliams <nadya@sdsc.edu>
2018-02-08 14:12:47 -0800

    rm dependence on nbcr roll

--- bootstrap.sh
+++ bootstrap.sh
@@ -54,13 +54,13 @@
 # @Copyright@
 #
 
-if [ ! -d $NBCRDEVEL ]; then
-    echo "Need to install roll 'nbcr' first"
-    exit 1
-fi
+#if [ ! -d $NBCRDEVEL ]; then
+#    echo "Need to install roll 'nbcr' first"
+#    exit 1
+#fi
 
 # set google url for dowloading sources
-. $NBCRDEVEL/bootstrap-values.sh
+export SURL="https://googledrive.com/host/0BwytB2hwlJ6Vbmx3TFNHY2I5OUk"
 
 # download sources
 . $ROLLSROOT/etc/bootstrap-functions.sh
